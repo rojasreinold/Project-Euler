286974b43c9b7fb807f5a7737f68cb4c9796cada
rojasereinold <rojasreinold@gmail.com>
2016-05-04 21:04:29 -0400

    Finished p12

--- p012/p12.py
+++ p012/p12.py
@@ -18,7 +18,7 @@ def listOfPrimes(size):
 
 def DivByTotal(num, primes):
     output = open("output.txt", 'w')
-    totalDiv = 0
+    totalDiv = 1
     i = 0
     divisors = []
     while True:
@@ -26,13 +26,18 @@ def DivByTotal(num, primes):
         output.write( "num is %d\n" % num)
         output.write( "primes[i] is %d\n" % primes[i])
         if num % primes[i] ==0 :
+            divisors.append(0)
+            while num % primes[i] ==0:
+                divisors[-1] += 1
+                num = num/primes[i]
             #output.write( " i is %d" % i)
             #output.write("num was %d" % num)
             #output.write( "prime was %d" % primes[i])
-            num = num/primes[i]
             #print "after division num was %d" %num
-            divisors.append(primes[i])
         elif num == 1 :
+            for c in range(len(divisors)):
+                totalDiv *= (divisors[c] +1)
+            print "total divs are %d" % totalDiv
             return totalDiv
         else:
             i += 1
@@ -41,7 +46,7 @@ triNumber = 1
 i = 2
 numDiv = 0
 listPrimes = []
-listPrimes = listOfPrimes(500)
+listPrimes = listOfPrimes(5000)
 
 triNumDoc = open("triNums.txt", 'w')
 while numDiv < 500:
@@ -49,3 +54,4 @@ while numDiv < 500:
     numDiv = DivByTotal(triNumber, listPrimes)
     triNumDoc.write("TriNum is %d \t\tdivby: %d\n" % (triNumber, numDiv))
     i += 1
+print triNumber
