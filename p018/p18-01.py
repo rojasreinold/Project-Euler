60003e416c16e9c191a939c0f5759b068efcc378
rojasereinold <rojasreinold@gmail.com>
2016-05-09 19:05:12 -0400

    Started writing classes for p18

---
+++ p018/p18-01.py
@@ -0,0 +1,20 @@
+class Pyramid:
+    __init__():
+       rows = 0
+    
+    def addRow(self):
+        rows += 1
+        
+
+class Row:
+    blocks = []
+    __init__(self, r):
+
+    def blockis(self, v):
+        blocks.append(Block, v)
+    def nextRow(self, r):
+        nextRow = r
+         
+class Block:
+    __init__(self,  v):
+        value = v
